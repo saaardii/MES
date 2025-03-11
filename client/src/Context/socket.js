803b8c38c8e94bfd5855c7395d1e642c3db717c5
sardinanb <ssardina@negribossi.com>
2025-03-11 09:16:24 +0100

    aggiornato versioning client/server; aggiornato indirizzo IP server in "socket.js"; modificato "machine.json", aggiunto variabile controlType;

--- client/src/Context/socket.js
+++ client/src/Context/socket.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import socketio from "socket.io-client";
-const ENDPOINT = "http://172.17.185.163:4000";
+const ENDPOINT = "http://172.17.185.137:4000";
 
 export const socket = socketio.connect(ENDPOINT);
 export const SocketContext = React.createContext();
